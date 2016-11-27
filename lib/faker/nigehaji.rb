9ed7acf315572dcce68f12dea4b1fd8d427889da
takiy33 <takiy33@users.noreply.github.com>
2016-11-27 10:54:14 +0900

    Structure original data

--- lib/faker/nigehaji.rb
+++ lib/faker/nigehaji.rb
@@ -2,10 +2,22 @@ require 'faker/nigehaji/version'
 
 module Faker
   module Nigehaji
-    NAME = ['森山 みくり', '津崎 平匡', '田中 安恵', '森山 栃男', '森山 桜', '風見 涼太', '沼田 頼綱', '日野 秀司', '土屋 百合', '梅原 ナツキ', '堀内 柚'].map(&:freeze).freeze
+    NAME = [
+      { first_name: 'みくり', last_name: '森山' },
+      { first_name: '平匡', last_name: '津崎' },
+      { first_name: '安恵', last_name: '田中' },
+      { first_name: '栃男', last_name: '森山' },
+      { first_name: '桜', last_name: '森山' },
+      { first_name: '涼太', last_name: '風見' },
+      { first_name: '頼綱', last_name: '沼田' },
+      { first_name: '秀司', last_name: '日野' },
+      { first_name: '百合', last_name: '土屋' },
+      { first_name: 'ナツキ', last_name: '梅原' },
+      { first_name: '柚', last_name: '堀内' }
+    ].map(&:freeze).freeze
 
     def self.name
-      NAME.sample
+      NAME.sample.values.join(' ')
     end
   end
 end
--- spec/faker/nigehaji_spec.rb
+++ spec/faker/nigehaji_spec.rb
@@ -3,7 +3,7 @@ describe Faker::Nigehaji do
     subject { Faker::Nigehaji.name }
 
     it do
-      is_expected.to be_an_element_of(Faker::Nigehaji::NAME)
+      is_expected.to be_an_element_of(Faker::Nigehaji::NAME.map{|hash| hash.values.join(' ') })
     end
   end
 end
